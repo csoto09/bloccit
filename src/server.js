ac98d1d4ad0a0feb91d6c8a868c1bfcd190d1b3f
Carlos J. Soto <csoto09@gmail.com>
2019-03-20 14:16:52 -0400

    WIP commit - refactoring and updating packages

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
-const http = require('http')
 const app = require('./app')
+const http = require('http')
 
 const port = normalizePort(process.env.PORT || '3000')
 app.set('port', port)
+
 const server = http.createServer(app)
 server.listen(port)
 
