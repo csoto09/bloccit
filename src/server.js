d34bd90d46877416080db836ddf411438e5ca9e3
Carlos J. Soto <csoto09@gmail.com>
2019-03-20 14:05:17 -0400

    WIP deploy to production

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const app = require('./app')
 
-const port = normalizePort(process.env.PORT || 3000)
+const port = normalizePort(process.env.PORT || '3000')
 app.set('port', port)
 const server = http.createServer(app)
 server.listen(port)
