15e0e602cd8aa0c82a8307b18ab97184786e6efe
Carlos J. Soto <csoto09@gmail.com>
2019-03-19 20:01:06 -0400

    intro to express assignment done

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,14 +1,29 @@
-const request = require("request");
-const server = require("../../src/server");
-const base = "http://localhost:3000/";
+const request = require('request')
+const server = require('../../src/server')
 
-describe("routes : static", () => {
-    describe("GET /", () => {
-        it("should return status code 200", (done) => {
-            request.get(base, (err, res, body) => {
-                expect(res.statusCode).toBe(200);
-                done();
-            });
-        });
-    });
-});
+const base = 'http://localhost:3000/'
+
+describe('routes : static', () => {
+  describe('GET /', () => {
+    it('should return status code 200', done => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200)
+        done()
+      })
+    })
+  })
+  describe('GET /marco', () => {
+    it('should return status code 200', done => {
+      request(`${base}marco`, (err, res, body) => {
+        expect(res.statusCode).toBe(200)
+        done()
+      })
+    })
+    it('should contain "polo"', done => {
+      request(`${base}marco`, (err, res, body) => {
+        expect(body).toBe('polo')
+        done()
+      })
+    })
+  })
+})
--- src/routes/static.js
+++ src/routes/static.js
@@ -1,8 +1,11 @@
 const express = require('express')
+
 const router = express.Router()
 
-router.get('/', (req,res,next) => {
-    res.send('Welcome to Bloccit!')
+router.get('/', (req, res, next) => {
+  res.send('Welcome to Bloccit!')
 })
-
+router.get('/marco', (req, res, next) => {
+  res.send('polo')
+})
 module.exports = router
