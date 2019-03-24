67bd1adf5e4c25a35e03bb8dd393d6bd098e972f
Carlos J. Soto <csoto09@gmail.com>
2019-03-23 21:56:10 -0400

    Post Resource Checkpoint, Part 2 - complete

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,8 +2,11 @@ module.exports = {
   init (app) {
     const staticRoutes = require('../routes/static')
     const topicRoutes = require('../routes/topics')
-    const adsRoutes = require('../routes/ads');
+    const adsRoutes = require('../routes/ads')
+    const postRoutes = require('../routes/posts')
+
     app.use(staticRoutes)
+    app.use(postRoutes)
     app.use(topicRoutes)
     app.use(adsRoutes)
   }
--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,4 +1,5 @@
 const Topic = require("./models").Topic;
+const Post = require('./models').Post
 
 module.exports = {
 
@@ -24,7 +25,13 @@ module.exports = {
     })
   },
   getTopic(id, callback) {
-    return Topic.findByPk(id)
+    // return Topic.findByPk(id)
+    return Topic.findByPk(id, {
+      include: [{
+        model: Post,
+        as: "posts"
+      }]
+    })
     .then((topic) => {
       callback(null, topic)
     })
