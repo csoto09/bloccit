4602b76b04fe842b25f9883e706cc007a71184f7
Carlos J. Soto <csoto09@gmail.com>
2019-03-30 22:20:56 -0400

    learn about validations in express

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -80,6 +80,25 @@ describe("routes : posts", () => {
        );
      });
  
+    it('should not create a new post that fails validation', (done) => {
+      const options = {
+        url: `${base}/${this.topic.id}/posts/create`,
+        form: {
+          title: "a",
+          body: "b"
+        }
+      }
+      request.post(options, (err, res, body) => {
+        Post.findOne({where: {title: "a"}})
+        .then((post) => {
+          expect(post).toBeNull();
+          done()
+        }).catch((err) => {
+          console.log(err);
+          done()
+        });
+      })
+    });
   });
 
   describe("GET /topics/:topicId/posts/:id", () => {
@@ -142,7 +161,8 @@ describe("routes : posts", () => {
         const options = {
           url: `${base}/${this.topic.id}/posts/${this.post.id}/update`,
           form: {
-            title: "Snowman Building Competition"
+            title: "Snowman Building Competition",
+            body: "I love watching them melt slowly."
           }
         };
         request.post(options,
