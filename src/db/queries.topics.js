08d6b9776d9d9020a9e89211086e997b6962c8de
Carlos J. Soto <csoto09@gmail.com>
2019-04-02 18:15:18 -0400

    authorization checkpoint complete

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -39,32 +39,58 @@ module.exports = {
       callback(err)
     })
   },
-  deleteTopic(id, callback){
-    return Topic.destroy({
-      where: {id}
-    })
+  deleteTopic(req, callback){
+    return Topic.findByPk(req.params.id)
     .then((topic) => {
-      callback(null, topic);
+      const authorized = new Authorizer(req.user, topic).destroy();
+
+      if(authorized) {
+        topic.destroy()
+        .then((res) => {
+          callback(null, topic);
+        });
+        
+      } else {
+        req.flash("notice", "You are not authorized to do that.")
+        callback(401);
+      }
     })
     .catch((err) => {
       callback(err);
-    })
+    });
   },
-  updateTopic(id, updatedTopic, callback){
-    return Topic.findByPk(id)
+  updateTopic(req, updatedTopic, callback){
+
+    // #1
+    return Topic.findById(req.params.id)
     .then((topic) => {
+
+    // #2
       if(!topic){
         return callback("Topic not found");
       }
-      topic.update(updatedTopic, {
-        fields: Object.keys(updatedTopic)
-      })
-      .then(() => {
-        callback(null, topic);
-      })
-      .catch((err) => {
-        callback(err);
-      });
+
+    // #3
+      const authorized = new Authorizer(req.user, topic).update();
+
+      if(authorized) {
+
+    // #4
+        topic.update(updatedTopic, {
+          fields: Object.keys(updatedTopic)
+        })
+        .then(() => {
+          callback(null, topic);
+        })
+        .catch((err) => {
+          callback(err);
+        });
+      } else {
+
+    // #5
+        req.flash("notice", "You are not authorized to do that.");
+        callback("Forbidden");
+      }
     });
-  },
+  }
 }
