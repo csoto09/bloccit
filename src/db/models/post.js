27b559824342bdc7b6c38a75766a5b75e1978cef
Carlos J. Soto <csoto09@gmail.com>
2019-04-11 20:52:21 -0400

    checkpoint 16 complete. create profile page and tests

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -46,15 +46,22 @@ module.exports = (sequelize, DataTypes) => {
         postId: post.id
       })
     })
-
-    Post.prototype.getPoints = function() {
-      if(this.votes.length === 0) return 0
-
-      return this.votes.map((v) => { return v.value }).reduce((prev, next) => { return prev + next })
-    }
-    Post.prototype.getFavoriteFor = function(userId){
-      return this.favorites.find((favorite) => { return favorite.userId == userId });
-    };
   };
+
+  Post.prototype.getPoints = function() {
+    if(this.votes.length === 0) return 0
+
+    return this.votes.map((v) => { return v.value }).reduce((prev, next) => { return prev + next })
+  }
+  Post.prototype.getFavoriteFor = function(userId){
+    return this.favorites.find((favorite) => { return favorite.userId == userId });
+  };
+  Post.addScope('lastFiveFor', (userId) => {
+    return {
+      where: {userId: userId},
+      limit: 5,
+      order: [['createdAt', 'DESC']]
+    }
+  })
   return Post;
 };
