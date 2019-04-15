5fa6b90af6c07923eec94c3f96f13ce67cfbdebc
Carlos J. Soto <csoto09@gmail.com>
2019-04-14 21:18:09 -0400

    fixing route for removing favorites

--- src/routes/favorites.js
+++ src/routes/favorites.js
@@ -4,6 +4,6 @@ const router = express.Router()
 const favoriteController = require('../controllers/favoriteController');
 
 router.post('/topics/:topicId/posts/:postId/favorites/create', favoriteController.create)
-router.post('/topics/:topicId/posts/:postId/favorites/destroy', favoriteController.destroy)
+router.post('/topics/:topicId/posts/:postId/favorites/:id/destroy', favoriteController.destroy)
 
 module.exports = router
